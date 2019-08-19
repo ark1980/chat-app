a3edb7003f1b150186b5b9a8748f41cff4584a96
Ali <ar.k@outlook.com>
2019-08-19 07:09:45 -0700

    add firebase configuration to the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA_ROJW0wK8qdSIUgSUPRCdbi9YYe1YJ2A",
+  authDomain: "chat-app-91d10.firebaseapp.com",
+  databaseURL: "https://chat-app-91d10.firebaseio.com",
+  projectId: "chat-app-91d10",
+  storageBucket: "chat-app-91d10.appspot.com",
+  messagingSenderId: "971348583635",
+  appId: "1:971348583635:web:ea343e8e31215400"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
